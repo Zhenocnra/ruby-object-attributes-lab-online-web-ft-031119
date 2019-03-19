ca7a503bf31f9eef41c6fe6848fe0d2dff5cb416
Zoncharne Lee <zoncharne@gmail.com>
2019-03-19 04:27:23 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,17 +1,18 @@
 class Person
  
-  def initialize(name)
-    @name = name
-  end
- 
   def name
     @name
   end
- 
-  def name=(new_name)
-    @name = new_name
+
+  def name=(name)
+    @name = name
   end
 
   def job
+    @job
+  end
+  
+  def job=(job)
+    @job = job
   end
 end
